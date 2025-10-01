d047e8cdbbb00ce0efc2fd729ac014c1ebc5898c
Jan Doskočil <30779172+Dook97@users.noreply.github.com>
2025-10-01 23:03:03 +0200

    inline lightline styling

--- init.lua
+++ init.lua
@@ -34,20 +34,6 @@ vim.pack.add({
 	{ src = "https://github.com/shirosaki/tabular", version = "fix_leading_spaces" }, -- multiline alignment
 })
 
--- lightline
-local theme = (os.getenv("XDG_DATA_HOME") or "~/.local/share")
-	.. "/nvim/site/pack/core/opt/lightline.vim/autoload/lightline/colorscheme/dook.vim"
-if vim.fn.filereadable(theme) == 0 then
-	print("Downloading custom lightline theme...")
-	vim.fn.system({
-		"curl",
-		"-fLo",
-		theme,
-		"--create-dirs",
-		"https://raw.githubusercontent.com/Dook97/nvim-config/main/lightline_colors.vim",
-	})
-end
-
 -- has to be defined in vimscript, idk why
 vim.cmd([[
 function! LightlineFileinfo()
@@ -71,12 +57,42 @@ endfunction
 function! LightlineReadonly()
 	return &readonly && &filetype !=# 'netrw' ? 'RO' : ''
 endfunction
+
+" custom style
+let s:p = {'normal': {}, 'inactive': {}, 'insert': {}, 'replace': {}, 'visual': {}, 'tabline': {}}
+
+let s:p.normal.left = [ ['darkestgreen', 'brightgreen', 'bold'], ['white', 'green'] ]
+let s:p.normal.middle = [ [ 'mediumgreen', 'darkestgreen' ] ]
+let s:p.normal.right = [ ['brightgreen', 'green', 'bold' ], ['brightgreen', 'darkestgreen'] ]
+let s:p.normal.error = [ [ 'gray9', 'brightestred' ] ]
+let s:p.normal.warning = [ [ 'gray1', 'yellow' ] ]
+
+let s:p.insert.left = [ ['darkestcyan', 'white', 'bold'], ['white', 'darkblue'] ]
+let s:p.insert.middle = [ [ 'mediumcyan', 'darkestblue' ] ]
+let s:p.insert.right = [ [ 'mediumcyan', 'darkblue' ], [ 'mediumcyan', 'darkestblue' ] ]
+
+let s:p.visual.left = [ ['darkred', 'brightorange', 'bold'], ['white', 'gray4'] ]
+let s:p.visual.middle = [ [ 'gray7', 'gray2' ] ]
+let s:p.visual.right = [ ['gray9', 'gray4'], ['gray8', 'gray2'] ]
+
+let s:p.replace.left = [ ['white', 'brightred', 'bold'], ['white', 'gray4'] ]
+let s:p.replace.middle = s:p.visual.middle
+let s:p.replace.right = s:p.visual.right
+
+let s:p.tabline.left = [ [ 'gray9', 'none'] ]
+let s:p.tabline.tabsel = [ [ 'gray10', 'darkestblue'] ]
+let s:p.tabline.middle = [ [ 'none', 'none', 'none', 'none' ] ]
+let s:p.tabline.right = [ [ 'none', 'none' ] ]
+
+let s:p.inactive.right = [ ['gray4', 'gray1'], ['gray4', 'gray0'] ]
+let s:p.inactive.middle = s:p.visual.middle
+let s:p.inactive.left = s:p.inactive.right[1:]
+
+let g:lightline#colorscheme#__custom__#palette = lightline#colorscheme#fill(s:p)
 ]])
 
-g["lightline#lsp#indicator_warnings"] = "W "
-g["lightline#lsp#indicator_errors"] = "E "
 g.lightline = {
-	colorscheme = "dook",
+	colorscheme = "__custom__",
 	component_function = {
 		mode = "LightlineMode",
 		readonly = "LightlineReadonly",
@@ -96,6 +112,8 @@ g.lightline = {
 	tabline_separator = { left = "", right = "" },
 	tabline_subseparator = { left = "", right = "" },
 }
+g["lightline#lsp#indicator_warnings"] = "W "
+g["lightline#lsp#indicator_errors"] = "E "
 vim.fn["lightline#lsp#register"]()
 
 -- netrw settings
